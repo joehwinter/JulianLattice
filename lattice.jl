431cc3f372f37167d14e016836382dbd55cef2cf
joewinter <joewinter1999@yahoo.co.uk>
2021-05-30 15:57:49 +0100

    Have added collision detection based on nearest neighbours Have added a movement function which allows each particle to random walk Have added the spread of infection for two adjacent particles changed how the lattice is inititalised and updated, a new lattice is created for a set of particles Split off everything into independent functions

--- lattice.jl
+++ lattice.jl
@@ -14,44 +14,136 @@ struct particle <: AbstractParticle
 	end 
 end 
 
-function updateLattice!(lattice :: Matrix, parts :: Vector)
-	for i in parts
-		if parts[i].infected == true
-			lattice[parts[i].x,parts[i].y] = -1 
+function createLattice(size :: Int64, parts :: Vector)
+	lattice = initaliseLattice(size,parts)
+	for part in parts
+		if part.infected == true
+			lattice[part.x,part.y] =  -1
 	    else
-			lattice[parts[i].x,parts[i].y] = 1
+			lattice[part.x,part.y] = 1
 		end
 	end
 	return lattice 
 end
 
-function collision(part :: particle)
+function createLattice(lattice :: Matrix, parts :: Vector)
+	size = length(lattice[1:end,1]) -2
+	lattice = initaliseLattice(size,parts)
+	for part in parts
+		if part.infected == true
+			lattice[part.x,part.y] =  -1
+	    else
+			lattice[part.x,part.y] = 1
+		end
+	end
+	return lattice 
+end
+
+function collision(part:: particle, lattice:: Matrix)
+	dirVectors = ([0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1])
+	collQuery = [false,false,false,false,false,false,false,false]
+	infection = false 
+	NBH = [part.pos + dirVector for dirVector in dirVectors]
+	allowedNBH = []
+	for i in 1:8
+		if lattice[NBH[i][1],NBH[i][2]] == 0 
+		elseif lattice[NBH[i][1],NBH[i][2]] == -1
+			infection = true
+			collQuery[i] = true
+		else 
+			collQuery[i] = true
+		end
+	end 
+	
+	for i in 1:8
+		if collQuery[i] == false
+			push!(allowedNBH,NBH[i])
+		end
+	end
+	return (allowedNBH, infection)
+
+end
+
+function move!(part:: particle, lattice:: Matrix)
+	coldata = collision(part,lattice)
+	if coldata[2] == true
+		part= particle(rand(coldata[1]),true)
+	else
+		part= particle(rand(coldata[1]),part.infected)
+	end
+return part 
+end 
+
+function dynamics!(lattice, parts)
+	for i in 1:length(parts)
+		parts[i] = move!(parts[i],lattice)
+		lattice = createLattice(lattice, parts)
+	end 
+println("dynamics")
+return (lattice,parts)
+end
+
+function initaliseParticles(size,num,initPercent)
+	parts = []
+	infParts = [] 
+	nonInfParts = [] 
+
+	for i in  1:round(num * initPercent)
+		push!(parts, particle(rand(2:size-1, (2)),true))
+		push!(infParts, last(parts))
+	end 
+	
+	for i in  1:(num - round(num * initPercent))
+		push!(parts, particle(rand(2:size-1, (2)),false))
+		push!(nonInfParts, last(parts))
+	end 
+
+	return (parts,nonInfParts,infParts)
+end
+
+function initaliseLattice(size,parts)
+	lattice = zeros( Int64, size +2 , size +2)
+	for i in 1:(size+2)
+		lattice[1,i] = 1 
+		lattice[size+2,i] = 1
+		lattice[i,1] = 1
+		lattice[i,size+2] = 1
+	end
+return lattice
+end
+
+function sim!(lattice,parts,n)
+	if  n == 0
+		return (lattice,parts)
+	else
+		println(n)
+		step = sim!(lattice,parts,n-1)
+		return dynamics!(step[1],step[2])
+		
+
+	end
 end
 
 
+#Initialising Packages
+using Colors,Plots
 
 
-#initialise the lattice 
+#input variables 
 size = 100 
 num = 20 
-lattice = zeros( Int64, size +2 , size +2)
-parts = []
-infParts = [] 
-nonInfParts = [] 
 initPercent = .30
 
-for i in  1:round(num * initPercent)
-	push!(parts, particle(rand(1:size, (2)),true))
-	push!(infParts, last(parts))
-end 
+#initialisation 
+partLists = initaliseParticles(size,num,initPercent)
+parts = partLists[1]
+nonInfParts = partLists[2]
+infParts = partLists[3]
+lattice = createLattice(size,parts)
 
-for i in  1:(num - round(num * initPercent))
-	push!(parts, particle(rand(1:size, (2)),true))
-	push!(nonInfParts, last(parts))
-end 
+heatmap(lattice,color = :redsblues)
 
-lattice = updateLattice!(lattice, parts)
-
+lattice100 = sim!(lattice,parts,100)
 
 
 
